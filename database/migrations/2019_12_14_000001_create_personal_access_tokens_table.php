c4f081c9dc8f4b06b6ea093c2efa1b2f1c7efd83
AgungPremaditya <premadityamandala@gmail.com>
2021-06-04 18:03:15 +0800

    Destinations Update

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,11 +14,8 @@ class CreatePersonalAccessTokensTable extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->increments('id');
-            // $table->morphs('tokenable');
-            $table->string('tokenable_type');
-            $table->uuid('tokenable_id');
-            $table->foreign('tokenable_id')->references('id')->on('users');
+            $table->bigIncrements('id');
+            $table->uuidMorphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
