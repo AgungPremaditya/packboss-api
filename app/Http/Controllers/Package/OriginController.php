18a67f37d3d5dbc8fc190936a97c165334105e5d
AgungPremaditya <premadityamandala@gmail.com>
2021-06-05 15:59:08 +0800

    Add Update and Delete on Origin

--- app/Http/Controllers/Package/OriginController.php
+++ app/Http/Controllers/Package/OriginController.php
@@ -94,7 +94,53 @@ class OriginController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $result = Origin::where('id', $id)->with('users')->first();
+
+        if (empty($result)) {
+            $response = [
+                'statusCode' => 404,
+                'messages' => 'not found',
+                'content' => null
+            ];
+
+            return response()->json($response, 404);    
+        }
+        
+        $validate = Validator::make($request->all(), [
+            'country_name' => 'required|string',
+            'province_name' => 'required|string',
+            'region_name' => 'required|string',
+            'postal_code' => 'required|string',
+            'detail_address' => 'required'
+        ]);
+
+        if($validate->fails()){
+            $response = [
+                'statusCode' => 422,
+                'messages' => $validate->errors(),
+                'content' => null
+            ];
+
+            return response()->json($response, 422);
+        }
+        
+        $data = [
+            'country_name' => $request->country_name,
+            'province_name' => $request->province_name,
+            'region_name' => $request->region_name,
+            'postal_code' => $request->postal_code,
+            'detail_address' => $request->detail_address
+        ];
+        
+        $result->update($data);
+
+        $response = [
+            'statusCode' => 200,
+            'messages' => 'success',
+            'content' => $result
+        ];
+
+        return response()->json($response);
     }
 
     /**
@@ -105,6 +151,26 @@ class OriginController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $result = Origin::where('id', $id)->with('users')->first();
+        
+        if (empty($result)) {
+            $response = [
+                'statusCode' => 404,
+                'messages' => 'not found',
+                'content' => null
+            ];
+
+            return response()->json($response, 404);    
+        }
+
+        $result->delete();
+
+        $response = [
+            'statusCode' => 200,
+            'messages' => 'succes',
+            'content' => null
+        ];
+
+        return response()->json($response);
     }
 }
